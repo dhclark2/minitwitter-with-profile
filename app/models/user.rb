e7de28e1f3a5488f7f03b99ca79c6c92eae5e5e9
Hamilton Clark <dhclark2@ncsu.edu>
2017-01-03 19:18:03 -0500

    added user_image column to users.rb table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  attachment :user_image
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 end
